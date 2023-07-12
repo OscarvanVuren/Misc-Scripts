0f432826acba21043fe3e5dba8d310ed6de71a07
OscarvanVuren <vanvureno@cardiff.ac.uk>
2023-07-12 14:54:02 +0100

    Updated traj2cellpar to include element lables for atomic positions

--- traj2cellpar.py
+++ traj2cellpar.py
@@ -28,3 +28,9 @@ def traj2cellpar(traj, image=-1, rounding=8):
       cd.write('\n')
       cd.write('Scaled Atomic Positions' + '\n')
       cd.write(str(np.round(atoms.get_scaled_positions(), rounding)) + '\n')
+      cd.write('\n')
+      cd.write('Scaled Atomic Positions Labeled by Element' + '\n')
+      for atom in range(len(unit.symbols)):
+        cd.write(unit.symbols[atom])
+        cd.write(str(np.round(atoms.get_scaled_positions()[atom], rounding)) + '\n')
+      
